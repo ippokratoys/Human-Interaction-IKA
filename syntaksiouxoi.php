a7eddc7d6438049857ea7dc5c6319fa51890fd93
ippokratoys <polydorosthasis@hotmail.com>
2018-01-08 16:26:54 +0200

    added ergazomenoi and suntaksiouxoi (the same will be for the rest) changed some on the main css

--- syntaksiouxoi.php
+++ syntaksiouxoi.php
@@ -5,50 +5,63 @@
     	<?php
     		include("refs.html")
     	?>
-       
+
     </head>
 <!-- Latest compiled and minified CSS -->
 <body>
-<?php 
+<?php
 	include("navbar.html");
 ?>
-	
-<br></br>
+
+<!-- <br></br> -->
 
 <div class="container">
+    <div class="row">
+        <h3>Συνταξιούχοι</h3>
+    </div>
+    <div class="row" id="workers-grid">
+    	<!-- <div class="col-md-1   col-sm-1">
+    	</div> -->
+        <div class="" >
 
-<div class="row">
-	<!-- <div class="col-md-1   col-sm-1">
-	</div> -->
-	<div class="col-md-offset-1  col-sm-offset-1"  >
+            <div class="col-md-4  col-sm-4" >
+        		<div class="card">
+        		  <img src="images/bebaiosh_suntaksewn.jpg" alt="Card image cap">
+        		  <div class="card-body">
+        		    <h4 class="card-title">Βεβαίωση Συντάξεων Για Φορολογική Χρήση</h4>
+        		    <p class="card-text">Μέσω της εφαρμογής αυτής μπορείται να εκτυπώσετε βεβαίωση συντάξεων για φορολογική χρήση συμπληρώνοντας τα κατάληλλα στοιχεία <small>(ή κάνοντας σύνδεση στον λογαρισμό σας)</small>.</p>
+        		    <a href="#" class="btn btn-primary" >Υπολογισμός  ενσήμων</a>
+        		  </div>
+        		</div>
+    		</div>
 
-		<div class="col-md-2  col-sm-2" >
-				<div class="card" style="width: 20rem;">
-				  <img class="card-img-top" src="images/funnydog.jpg" alt="Card image cap">
-				  <div class="card-body">
-				    <h4 class="card-title">Εκτύπωση βεβαίωσης συντάξεων</h4>
-				    <p class="card-text">.......................</p>
-				    <a href="#" class="btn btn-primary">Go somewhere</a>
-				  </div>
-				</div>
-		</div>
-		<div class="col-md-2   col-sm-2">
+            <div class="col-md-4  col-sm-4" >
+        		<div class="card">
+        		  <img src="images/calculator.jpg" alt="Card image cap">
+        		  <div class="card-body">
+        		    <h4 class="card-title">Ενημερωτικό Σημείωμα Συντάξεων</h4>
+                    <p class="card-text">Μέσω της εφαρμογής αυτής μπορείται να εκτυπωσετε ενημερωτικό σημείωμα συντάξεων</p>
+        		    <a href="#" class="btn btn-primary" >Υπολογισμός χρόνων για σύνταξη</a>
+        		  </div>
+        		</div>
+    		</div>
 
-			<div class="card" style="width: 20rem;">
-			  <img class="card-img-top" src="images/funnydog.jpg" alt="Card image cap">
-			  <div class="card-body">
-			    <h4 class="card-title">Αποστολή βεβαίωσης στο ηλεκτρονικό ταχυδρομειο</h4>
-			    <p class="card-text">......................</p>
-			    <a href="#" class="btn btn-primary">Go somewhere</a>
-			  </div>
-			</div>
+            <div class="col-md-4  col-sm-4" >
+        		<div class="card">
+        		  <img src="images/calculator.jpg" alt="Card image cap">
+        		  <div class="card-body">
+        		    <h4 class="card-title">Ενημερωτικό Σημείωμα Συντάξεων</h4>
+                    <p class="card-text">Μέσω της εφαρμογής αυτής μπορείται να εκτυπωσετε ενημερωτικό σημείωμα συντάξεων</p>
+        		    <a href="#" class="btn btn-primary" >Υπολογισμός χρόνων για σύνταξη</a>
+        		  </div>
+        		</div>
+    		</div>
+
 		</div>
-		
-		</div>
-	</div>	
+	</div>
 </div>
 
-<?php 
+<?php
 	include("footer.html");
 ?>
 
