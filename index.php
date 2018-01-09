dd3911a5771a99b790d27ea77bd0fcd25cf63b99
ippokratoys <polydorosthasis@hotmail.com>
2018-01-09 02:26:12 +0200

    minor changes to index (add text)

--- index.php
+++ index.php
@@ -81,39 +81,36 @@
 <div class="row" id="home-text">
     <hr></hr>
     <div class="col-sm-6">
-        <p>
-            Καλωσήρθατε στον ιστοχώρο του IKA μέσω της πλατφόρμας αυτής μπορείται να χρησημοποιείσεται της ψηφιακές υπηρεσίες του ika.
-            Ανάλογα με το τι υπηρεσία σας ενδιφέρει, πατήστε την ανάλογη ομάδα χρηστών στην οποία ανήκει και πλοηγηθείται στις διαθέσημες υπηρεσίες.
-            Αν δεν βρείτε αυτό που ψάχνεται μπορείται να χρησημοποιείσεται την αναζήτηση καθώς και να εποικινωνισετε μαζί μας με του διαθεσημου τρόπους (μεσο της αντιστοιχης σελίδας).
-        </p>
     </div>
     <div class="col-sm-6">
     </div>
 </div>
 <div class="row">
-	<div class="col-lg-3 col-md-3   col-sm-3">
-	</div>
-	<div class="  col-lg-3 col-md-3   col-sm-3">
+	<div class="  col-lg-6 col-md-6   col-sm-6">
 
 		<div class="card" >
-		 <a href=#> <img class="card-img-top" src="images/faq.png" alt="Card image cap" style="width: 100%; height: 100%;"></a>
 		  <div class="card-block">
-
-
-		    <a href="#" class="btn btn-primary" style="width: 100%;"><h4>Συχνές Ερωτήσεις</h4></a>
-
+              <p class="text-welcome">
+                  Καλωσήρθατε στον ιστοχώρο του ΙΚΑ μέσω της πλατφόρμας αυτής μπορείται να χρησημοποιείσεται της ψηφιακές υπηρεσίες του ΙΚΑ.
+                  Ανάλογα με το τι υπηρεσία σας ενδιφέρει, πατήστε την ανάλογη ομάδα χρηστών στην οποία ανήκει και πλοηγηθείται στις διαθέσημες υπηρεσίες.
+              </p>
+              <a href=#> <img class="card-img-top" src="images/faq.png" alt="Card image cap" style="min-width:100% ; height:100% ; max-height:300px"></a>
+		      <a href="#" class="btn btn-primary" style="width: 100%;"><h4>Συχνές Ερωτήσεις</h4></a>
 
 		  </div>
 		</div>
 	</div>
-	<div class="col-lg-3 col-md-3   col-sm-3">
 
+	<div class="  col-lg-6 col-md-6   col-sm-6">
 		<div class="card" >
-		  <a href="#"><img class="card-img-top" src="images/map.jpg" alt="Card image cap" style="width: 100%; height: 80%;"></a>
 		  <div class="card-block">
+              <p class="text-welcome">
+                  Αν δεν βρείτε αυτό που ψάχνεται μπορείται να χρησημοποιείσεται την αναζήτηση καθώς και να εποικινωνισετε μαζί μας με του διαθεσημου τρόπους (μεσο της αντιστοιχης σελίδας).
+                  Βρείτε την ακριβή τοποθεσία των καταστημάτων ΙΚΑ που σας ενδιαφέρουν, καθώς και τα τηλεφωνα επικοινωνίας και τα ωράρια λειτουργίας.
 
-  		    <a href="#" class="btn btn-primary" style="width: 100%; height: 80%;"><h4>Επικοινωνία & <br> Δίκτυο Καταστήματων</h4></a>
-
+              </p>
+              <a href=#> <img class="card-img-top" src="images/map.jpg" alt="Card image cap" style="min-width:100% ; height:100% ; max-height:300px"></a>
+		      <a href="#" class="btn btn-primary" style="width: 100%;"><h4>Επικοινωνία & Δίκτυο Καταστήματων</h4></a>
 		  </div>
 		</div>
 	</div>
