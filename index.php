00644a1a6ca5e8c44a21a41b8f337317af04b73e
marinosagap <minosagap@gmail.com>
2018-01-10 21:46:19 +0200

    menu sto navbar

--- index.php
+++ index.php
@@ -72,134 +72,26 @@
 
 	<div class="col-lg-6 col-md-6  col-sm-6" >
 		<div class="card" >
-		  <a href="syntaksiouxoi.php"><img class="card-img-top" src="images/genika.jpg" alt="Card image cap" style="width: 100%; height:100% ;      object-fit: cover;max-height: 300px;"></a>
+		  <a href="genikes_plhrofories.php"><img class="card-img-top" src="images/genika.jpg" alt="Card image cap" style="width: 100%; height:100% ;      object-fit: cover;max-height: 300px;"></a>
 		  <div class="card-body">
 
-		    <a href="syntaksiouxoi.php" class="btn btn-primary" style="width: 100%;"><h4>Γενικές υπηρεσίες</h4></a>
+		    <a href="genikes_plhrofories.php" class="btn btn-primary" style="width: 100%;"><h4>Γενικές Πληροφορίες</h4></a>
+
 		  </div>
 		</div>
 	</div>
 </div>
 
-<div class="row">
-	<div class="  col-lg-6 col-md-6   col-sm-6">
 
-		<div class="card" >
-		  <div class="card-block">
-              <p class="text-welcome">
-                  Καλωσήρθατε στον ιστοχώρο του ΙΚΑ μέσω της πλατφόρμας αυτής μπορείται να χρησημοποιείσεται της ψηφιακές υπηρεσίες του ΙΚΑ.
-                  Ανάλογα με το τι υπηρεσία σας ενδιφέρει, πατήστε την ανάλογη ομάδα χρηστών στην οποία ανήκει και πλοηγηθείται στις διαθέσημες υπηρεσίες.
-              </p>
-              <a href=#> <img class="card-img-top" src="images/faq.png" alt="Card image cap" style="min-width:100% ; height:100% ; max-height:300px"></a>
-		      <a href="#" class="btn btn-primary" style="width: 100%;"><h4>Συχνές Ερωτήσεις</h4></a>
-
-		  </div>
-		</div>
-	</div>
-
-	<div class="  col-lg-6 col-md-6   col-sm-6">
-		<div class="card" >
-		  <div class="card-block">
-              <p class="text-welcome">
-                  Αν δεν βρείτε αυτό που ψάχνεται μπορείται να χρησημοποιείσεται την αναζήτηση καθώς και να εποικινωνισετε μαζί μας με του διαθεσημου τρόπους (μεσο της αντιστοιχης σελίδας).
-                  Βρείτε την ακριβή τοποθεσία των καταστημάτων ΙΚΑ που σας ενδιαφέρουν, καθώς και τα τηλεφωνα επικοινωνίας και τα ωράρια λειτουργίας.
-              </p>
-              <a href=#> <img class="card-img-top" src="images/map.jpg" alt="Card image cap" style="min-width:100% ; height:100% ; max-height:300px"></a>
-		      <a href="#" class="btn btn-primary" style="width: 100%;"><h4>Επικοινωνία & Δίκτυο Καταστήματων</h4></a>
-		  </div>
-		</div>
-	</div>
-
-
-</div>
 
 <br></br>
-<div class="row">
-	<div class="col-md-2   col-sm-2"></div>
-	<div class="col-md-2   col-sm-2">
 
-		<div class="card" >
-		  <img class="card-img-top" src="images/funnydog.jpg" alt="Card image cap" style="width: 10rem; height: 10rem;">
-		  <div class="card-block">
-		    <h4 class="card-title">Anakoinwseis</h4>
-
-		    <p class="card-text">.......................</p>
-		    <a href="#" class="btn btn-primary">Go somewhere</a>
-
-
-		  </div>
-		</div>
-	</div>
-	<div class="col-md-2   col-sm-2">
-
-		<div class="card" >
-		  <img class="card-img-top" src="images/funnydog.jpg" alt="Card image cap" style="width: 10rem; height: 10rem;">
-		  <div class="card-block">
-		    <h4 class="card-title">Nomosxedia</h4>
-
-		    <p class="card-text">.......................</p>
-  		    <a href="#" class="btn btn-primary">Go somewhere</a>
-
-		  </div>
-		</div>
-	</div>
-
-	<div class="col-md-2   col-sm-2">
-
-		<div class="card" >
-		  <img class="card-img-top" src="images/funnydog.jpg" alt="Card image cap" style="width: 10rem; height: 10rem;">
-		  <div class="card-block">
-		    <h4 class="card-title">Xrhsimes plhrofories</h4>
-
-		    <p class="card-text">.......................</p>
-		    <a href="#" class="btn btn-primary">Go somewhere</a>
-
-
-		  </div>
-		</div>
-	</div>
-	<div class="col-md-2   col-sm-2">
-
-		<div class="card" >
-		  <img class="card-img-top" src="images/funnydog.jpg" alt="Card image cap" style="width: 10rem; height: 10rem;">
-		  <div class="card-block">
-		    <h4 class="card-title">Alles yphresies</h4>
-
-		    <p class="card-text">.......................</p>
-  		    <a href="#" class="btn btn-primary">Go somewhere</a>
-
-		  </div>
-		</div>
-	</div>
-</div>
       <br></br>
 </div>
-<nav class="navbar navbar-default fixed-bottom" >
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Sitemap</a>
-      <br></br>
-      <a class="navbar-brand" href="#">Epikoinwnia</a>
-      <br></br>
+<?php
+	include("footer.html");
+?>
 
-      <a class="navbar-brand" href="#">Boitheia</a>
-    </div>
-
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
-
-
-      <a>IKA LOGO 1821-2017</a>
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
-</nav>
 <!--
 <form class="form-inline" action="yolo.php" method="POST">
   <div class="form-group">
@@ -212,6 +104,5 @@
   </div>
   <button type="submit" class="btn btn-default">Send invitation</button>
 </form> -->
-</div>
 </body>
 </html>
