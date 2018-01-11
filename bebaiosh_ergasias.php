e908b31b2713128039983965a2b369837252f734
ippokratoys <polydorosthasis@hotmail.com>
2018-01-11 18:51:27 +0200

    bebaiwsh ergasias

---
+++ bebaiosh_ergasias.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php
+    include("refs.html");
+  ?>
+  <!-- Custom styles for this template -->
+
+</head>
+      <link rel="stylesheet" href="css/register.css">
+
+<body>
+
+
+
+    <div id="wrapper">
+
+
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+          <?php
+            include("navbar.html");
+          ?>
+
+
+        <!-- /#page-content-wrapper -->
+    <!-- /#wrapper -->
+         <div class="container">
+          <h3>Βεβαίωση Εργασίας</h3>
+        <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.php">Αρχική</a></li>
+        <li class="breadcrumb-item">Εργαζόμενοι</li>
+        <li class="breadcrumb-item active">Υπολογισμός Ενσύμων</li>
+
+        </ol>
+            <div class="row main">
+                <div class="panel-heading">
+                   <!-- <div class="panel-title text-center">
+                        <h1 class="title">ΙΚΑ ΥΠΟΛΟΓΙΣΜΟΣ ΕΝΣΥΜΩΝ</h1>
+                        <hr />
+                    </div> -->
+                </div>
+                <div class="main-login main-center">
+                  <form class="form-horizontal" action="bebaiosh_ergasias_page.php" method="post" id="bebaiosh_ergasias">
+
+                   <div class="form-group">
+                        <label for="amka" class="cols-sm-2 control-label">ΑΜΚΑ</label>
+                        <div class="cols-sm-10">
+                            <div class="input-group">
+                                <input required="true" type="number" class="form-control" name="amka" id="amka"  placeholder="Εισάγεται AMKA"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="afm" class="cols-sm-2 control-label">ΑΦΜ</label>
+                        <div class="cols-sm-10">
+                            <div class="input-group">
+                                <input required="true" type="number" class="form-control" name="ΑΦΜ" id="afm"  placeholder="Εισάγεται ΑΦΜ"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="tautotita" class="cols-sm-2 control-label">Αριθμός Ταυτότητας</label>
+                        <div class="cols-sm-10">
+                            <div class="input-group">
+
+                                <input required="true" type="text" class="form-control" name="tautotita" id="tautotita"  placeholder="Αριθμός Ταυτότητας"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="etoi" class="cols-sm-2 control-label">Επώνυμο</label>
+                        <div class="cols-sm-10">
+                            <div class="input-group">
+                                <input required="true" type="text" class="form-control" name="surname" id="surname"  placeholder="Εισάγεται το επώνυμο σας"/>
+                            </div>
+                        </div>
+                    </div>
+
+
+                        <div class="form-group ">
+                            <!-- <button id="login-button" type="button" class="btn btn-primary btn-lg btn-block login-button">Σύνδεση</button> -->
+                            <input type="submit" value="Εκτύπωση" class="btn btn-primary btn-lg btn-block login-button"></input>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <script src="js/ensima.js"></script>
+        <script src="js/bebaiosh_ergasias.js"></script>
+</body>
+
+</html>
---
+++ bebaiosh_ergasias_page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="el">
+
+    <head>
+      <!-- Custom styles for this template -->
+      <link rel="stylesheet" href="css/bebaiosh.css">
+      <title>Βεβαίωση Εγασίας - ΙΚΑ</title>
+    </head>
+
+    <h1>Ικα</h1>
+    <h2> Βεβαίωση Εργασίας</h2>
+    <hr></hr>
+    <p> <strong> Όνομα : </strong> </p>
+    <p> <strong> Επώνυμο : </strong> </p>
+    <p> <strong> Α.Φ.Μ : </strong> </p>
+    <p> <strong> Α.Μ.Κ.Α : </strong> </p>
+    <br></br>
+    <p> <strong> Α.Φ.Μ Εταιρίας : </strong> </p>
+
+    <p> Ο παραπάνω ασφαλισμένος δεν εργάζεται σε κάποια εταιρία αυτήν την στιγμή.</p>
+</html>
